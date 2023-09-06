2b0e6b288fa1d6b93f97191bca5c064b7f979cca
Trình <trinhbatquan2001@gmail.com>
2023-09-06 15:30:44 +0700

    change readme.md

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,13 @@
 //tsdrpfc
 import * as React from 'react';
+import { useRouter } from 'next/router';
 
 export interface IAboutPageProps {
 }
 
-export default function AboutPage (props: IAboutPageProps) {
+export default function AboutPage(props: IAboutPageProps) {
+  const router = useRouter();
+  console.log(router)
   return (
     <div>
       <p>Hello</p>
